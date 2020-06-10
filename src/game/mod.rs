f2de2365d5ae35523ef8ac9b48867c902ad6312d
Igoru <igorbreno95@outlook.com>
2020-06-10 13:37:29 -0400

    Porta o jogo para o crossterm e faz compilar no Windows
    * Testado apenas no Windows 10 build 2004

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,27 +1,29 @@
 pub mod snake;
 
-extern crate termion;
+use crossterm::terminal::{self, Clear, ClearType};
+use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
+    execute, queue, style,
+};
 
-use termion::input::{TermRead};
-use termion::raw::IntoRawMode;
-use std::io::{Write, stdout, stdin};
-use self::termion::event::Key;
-use std::{thread, time};
-use std::sync::mpsc::{channel, TryRecvError};
-use std::borrow::BorrowMut;
 use rand::rngs::ThreadRng;
 use rand::Rng;
+use std::borrow::BorrowMut;
+use std::io::{stdin, stdout, Write};
+use std::sync::mpsc::{channel, TryRecvError};
+use std::{thread, time};
+
 use crate::game::snake::Point;
-use self::termion::terminal_size;
 
 pub fn main() {
-    let mut stdin = stdin();
-    let mut stdout = stdout().into_raw_mode().unwrap();
+    let mut stdout = stdout();
 
-    let (term_width, term_height) = terminal_size().unwrap();
+    terminal::enable_raw_mode().unwrap();
 
-    write!(stdout, "{}{}", termion::clear::All, termion::cursor::Hide).unwrap();
-    stdout.flush().unwrap();
+    let (term_width, term_height) = terminal::size().unwrap();
+
+    execute!(stdout, Clear(ClearType::All), cursor::Hide).unwrap();
 
     let mut snake = snake::Snake::new(1, 1, term_width, term_height);
     snake.walk(snake::WalkDirection::Right, true);
@@ -32,10 +34,10 @@ pub fn main() {
 
     let (tx, rx) = channel();
 
-    thread::spawn(move || {
+    thread::spawn(move || loop {
         loop {
-            for key in stdin.borrow_mut().keys() {
-                tx.send(key.unwrap()).unwrap();
+            if let Event::Key(key_event) = event::read().unwrap() {
+                tx.send(key_event).unwrap();
             }
         }
     });
@@ -45,39 +47,38 @@ pub fn main() {
 
     // Game Logic
     let mut lose: bool;
-    let mut last_move = Key::Right;
+    let mut last_move = KeyCode::Right;
     let mut food = snake::Point::new(10, 10);
 
     loop {
         match rx.try_recv() {
-            Ok(evt) => {
-                match evt {
-                    Key::Esc | Key::Ctrl('c') => {
-                        break;
-                    }
-                    _ => {
-                        let (new_snake, grow, status_lose) = move_snake(snake, evt, &food);
-
-                        snake = new_snake;
-
-                        if grow
-                        {
-                            food = generate_new_food(rng, term_width, term_height);
-                        }
-
-                        last_move = evt;
-
-                        lose = status_lose;
-                    }
+            Ok(key_event) => match key_event {
+                KeyEvent {
+                    code: KeyCode::Char('c'),
+                    modifiers: KeyModifiers::CONTROL,
+                } => {
+                    break;
                 }
-            }
+                KeyEvent { code, .. } => {
+                    let (new_snake, grow, status_lose) = move_snake(snake, code, &food);
+
+                    snake = new_snake;
+
+                    if grow {
+                        food = generate_new_food(rng, term_width, term_height);
+                    }
+
+                    last_move = code;
+
+                    lose = status_lose;
+                }
+            },
             Err(TryRecvError::Empty) => {
                 let (new_snake, grow, status_lose) = move_snake(snake, last_move, &food);
 
                 snake = new_snake;
 
-                if grow
-                {
+                if grow {
                     food = generate_new_food(rng, term_width, term_height);
                 }
 
@@ -94,56 +95,44 @@ pub fn main() {
 
         // Render
         // Clear All
-        write!(stdout, "{}", termion::clear::All).unwrap();
+        execute!(stdout, Clear(ClearType::All)).unwrap();
 
         // Food
-        write!(
-            stdout, "{}X",
-            termion::cursor::Goto(food.x, food.y)
-        ).unwrap();
+        execute!(stdout, cursor::MoveTo(food.x, food.y), style::Print("x")).unwrap();
 
         // Snake body
         for point in snake.body.iter() {
-            write!(
-                stdout, "{}o",
-                termion::cursor::Goto(point.x, point.y)
-            ).unwrap();
+            queue!(stdout, cursor::MoveTo(point.x, point.y), style::Print("o")).unwrap();
         }
 
         stdout.flush().unwrap();
     }
 
-    write!(stdout, "{}{}Pontuacao: {}{}",
-           termion::cursor::Goto(1, 1),
-           termion::clear::All,
-           snake.body.len(),
-           termion::cursor::Show
-    ).unwrap();
+    execute!(
+        stdout,
+        terminal::Clear(ClearType::All),
+        cursor::MoveTo(1, 1),
+        style::Print(format!("Pontuação: {}\n", snake.body.len()))
+    )
+    .unwrap();
 }
 
 fn generate_new_food(mut rng: ThreadRng, width: u16, height: u16) -> Point {
-    snake::Point::new(
-        rng.gen_range(0, width),
-        rng.gen_range(0, height),
-    )
+    snake::Point::new(rng.gen_range(0, width), rng.gen_range(0, height))
 }
 
-fn move_snake(mut snake: snake::Snake, key: Key, food: &snake::Point) -> (snake::Snake, bool, bool) {
+fn move_snake(
+    mut snake: snake::Snake,
+    key: KeyCode,
+    food: &snake::Point,
+) -> (snake::Snake, bool, bool) {
     let can_grow = snake.body.front().unwrap() == food;
 
     let lose = match key {
-        Key::Up | Key::Char('w') => {
-            snake.walk(snake::WalkDirection::Up, can_grow)
-        }
-        Key::Down | Key::Char('s') => {
-            snake.walk(snake::WalkDirection::Down, can_grow)
-        }
-        Key::Left | Key::Char('a') => {
-            snake.walk(snake::WalkDirection::Left, can_grow)
-        }
-        Key::Right | Key::Char('d') => {
-            snake.walk(snake::WalkDirection::Right, can_grow)
-        }
+        KeyCode::Up | KeyCode::Char('w') => snake.walk(snake::WalkDirection::Up, can_grow),
+        KeyCode::Down | KeyCode::Char('s') => snake.walk(snake::WalkDirection::Down, can_grow),
+        KeyCode::Left | KeyCode::Char('a') => snake.walk(snake::WalkDirection::Left, can_grow),
+        KeyCode::Right | KeyCode::Char('d') => snake.walk(snake::WalkDirection::Right, can_grow),
         _ => (true),
     };
     (snake, can_grow, !lose)
